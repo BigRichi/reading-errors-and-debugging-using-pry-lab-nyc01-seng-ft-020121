73f8f51a90f6c72f3eafb83d5f7bda4635169925
Richard Adule <big.richi@outlook.com>
2021-01-22 16:52:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,9 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
-
-
-p start_game("richard", 'Tiffany')
-p play_game("tilso", "Bre")
